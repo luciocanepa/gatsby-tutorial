21f0804d955fb0f3a3775ce95162b4989b3bb131
Lucio Canepa <luciocanepa@macbook-pro-di-lucio.home>
2021-08-24 19:21:38 +0200

    Tutorial completato

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,16 @@ module.exports = {
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-transformer-remark",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
     },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
   ],
 };
